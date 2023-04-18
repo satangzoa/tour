42c2d4b82734c80327eb2d1d304aeee4f9cc0376
A <A@DESKTOP-NKSGQVR>
2023-04-18 17:22:43 +0900

    myview에message띄우기

---
+++ src/main/java/com/oraclejava/tour/MyController.java
@@ -0,0 +1,35 @@
+package com.oraclejava.tour;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+/**
+ * MyController
+ * @author Ps
+ *
+ */
+@Controller
+public class MyController {
+
+	@RequestMapping("/")
+	public ModelAndView handleTop(ModelAndView mav) {
+		
+		mav.addObject("mymessage", "Hello😜😜");
+		mav.setViewName("myview");
+		return mav;
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
---
+++ src/test/java/com/oraclejava/tour/TourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oraclejava.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
